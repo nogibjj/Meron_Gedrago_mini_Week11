5dc64dd5ca1fc96414b020ca688e2e743d5eb3d1
merongedrago@gmail.com <merongedrago@gmail.com>
2024-11-27 01:13:19 +0000

    fixing main

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ Main cli or app entry point
 """
 
 from mylib.extract import extract
-from Notebook_Folder.transform_load import load
-from Notebook_Folder.query_viz import query_transform, viz
+from mylib.transform_load import load
+from mylib.query_viz import query_transform, viz
 import os
 
 
